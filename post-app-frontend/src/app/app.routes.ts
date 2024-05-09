f51b2f4785ebc6b3328d171d3d49e1ad947aabc6
Mbacke Mbaye <mbackembaye74@gmail.com>
2024-05-09 19:46:39 +0000

    update

--- post-app-frontend/src/app/app.routes.ts
+++ post-app-frontend/src/app/app.routes.ts
@@ -5,22 +5,29 @@ export const routes: Routes = [
   {
     path:'',
     component: MainComponent,
+    children: [
+      {
+        path: '',
+        loadComponent: () => import('./views/main/main-posts/main-posts.component').then(m => m.MainPostsComponent),
+      },
+      {
+        path: 'posts',
+        loadComponent: () => import('./views/main/main-posts/main-posts.component').then(m => m.MainPostsComponent),
+      },
+      {
+        path: 'post/create',
+        loadComponent: () => import('./views/main/main-create-post/main-create-post.component').then(m => m.MainCreatePostComponent),
+      },
+      {
+        path: 'post/show/:id',
+        loadComponent: () => import('./views/main/main-show-post/main-show-post.component').then(m => m.MainShowPostComponent),
+      },
+      {
+        path: 'post/edit/:id',
+        loadComponent: () => import('./views/main/main-update-post/main-update-post.component').then(m => m.MainUpdatePostComponent),
+      }
+    ]
   },
-  {
-    path: 'posts',
-    loadComponent: () => import('./views/main/main-posts/main-posts.component').then(m => m.MainPostsComponent),
-  },
-  {
-    path: 'post/create',
-    loadComponent: () => import('./views/main/main-create-post/main-create-post.component').then(m => m.MainCreatePostComponent),
-  },
-  {
-    path: 'post/show/:id',
-    loadComponent: () => import('./views/main/main-show-post/main-show-post.component').then(m => m.MainShowPostComponent),
-  },
-  {
-    path: 'post/edit/:id',
-    loadComponent: () => import('./views/main/main-update-post/main-update-post.component').then(m => m.MainUpdatePostComponent),
-  }
+
 
 ];
